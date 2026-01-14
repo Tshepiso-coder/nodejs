a38eb839650b864816b9e2fd6aff54c2e64cb609
Tshepiso <lesegograce5@gmail.com>
2026-01-14 08:33:40 +0200

    initial commit

---
+++ Unit0/Lesson01/lesson01.js
@@ -0,0 +1,12 @@
+3 + 3
+3 / 0
+console.log("Hello, Universe!");
+let name = "Jon Wexler";
+console.log(name);
+class Goat {eat(foodType) {
+class Goat {eat(foodType) {
+class Goat {eat(foodType) {console.log(`I love eating ${foodType}`);
+class Goat {eat(foodType) {console.log(`I love eating ${foodType}`);}
+class Goat {eat(foodType) {console.log(`I love eating ${foodType}`); }}
+let billy = new Goat();
+billy.eat("tin cans");
