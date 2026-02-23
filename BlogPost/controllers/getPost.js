eea970be755e43d486a203326b066a6746d27233
Tshepiso <lesegograce5@gmail.com>
2026-02-23 21:19:11 +0200

    finished lesson 13

--- BlogPost/controllers/getPost.js
+++ BlogPost/controllers/getPost.js
@@ -1,7 +1,7 @@
 const BlogPost = require("../models/BlogPost");
 
 module.exports = async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id);
+    const blogpost = await BlogPost.findById(req.params.id).populate("userid");
     console.log(`Single post: ${blogpost}`);    
     res.render("post", {blogpost});
 }
--- BlogPost/controllers/storePost.js
+++ BlogPost/controllers/storePost.js
@@ -1,11 +1,11 @@
 const BlogPost = require("../models/BlogPost");
 const path = require("path");
 
-module.exports = async (req, res) => {
-    req.body["username"] = "Tshepiso";    
+module.exports = async (req, res) => {   
     let image = req.files.image;
     const imagePath = "/img/" + image.name;  
     req.body["image"] = imagePath;
+    req.body["userid"] = req.session.userId;
     console.log(req.body);
     image.mv(path.resolve(__dirname,'./public/img',image.name),async (error)=>{ 
     await BlogPost.create(req.body);
